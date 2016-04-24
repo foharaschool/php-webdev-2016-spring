39e8fad2c4296383c636a89fb9eb5f52e1737f6d
foharaschool <fohara@madisoncollege.edu>
2016-04-24 18:09:22 +0000

    labch12 and setup for project 4

---
+++ projects/project04/index.php
@@ -0,0 +1,26 @@
+<?php
+    // Start the session
+    session_start();
+    
+    // Kill any active sessions
+    session_unset();
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Fred's Project Backlog</title>
+<!-- jQuery -->
+<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="style3.css">
+
+</head>
+<body>
+<?php
+    require_once('connectvars4.php');
+?>
